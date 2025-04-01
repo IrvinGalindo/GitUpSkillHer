dd2b47c983b8c071813392b5406b3b851950a363
Irvin Galindo <=>
2025-03-31 20:47:59 -0600

    mas cambios 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
-        System.out.println("My Name is Irvin");
-
-        System.out.println("Bye");
+        System.out.println("Hello world!1");
+        System.out.println("Hello world!1");
+        System.out.println("Hello world!1");
+        System.out.println("Hello world!1");
+        System.out.println("Hello world!1");
 
     }
 }
